a869c249643b6ff32ff6aa59b014f488b62bd7d5
Hira797 <48818522+Hira797@users.noreply.github.com>
2019-07-11 17:42:52 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+let resturant = [
+    {
+        name: "Kababjees",
+        city: "Karachi",
+        address: "Shaheed-E-Millat Do Darya , North Nazimabad, Karachi 74550, Pakistan",
+        phoneNumber: "+92 21 111 666 111",
+        webSite: "http://www.kababjees.com",
+        menu: {
+            fastfood: {
+                ChickenBurger: '350rs',
+                Broast: '400rs',
+                chickenSandwich: '550rs',
+                curlyFries: '150rs',
+                chickenNuggets: '350rs',
+                pizza: '899rs',
+                pizzaFries: '500rs',
+            },
+            desiFood: {
+                butterChicken:'500rs',
+                roganJosh: '900rs',
+                tandooriChicken: '800rs',
+                malaiKofta:'999rs',
+                masalaChicken:'650',
+                matarPaneer:'560',
+            },
+            seaFood: {
+                fish:'680rs',
+                prawn:'1100rs',
+                lobster: '990rs',
+                crab: '950rs',
+                shirmp: '1200rs',
+
+            },
+            
+        }
+        
+    }
+
+]
