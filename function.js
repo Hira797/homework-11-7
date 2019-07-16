640feb47cb578a7d0eb94d5e716f6867518cd3dc
Hira797 <48818522+Hira797@users.noreply.github.com>
2019-07-16 16:06:37 +0500

    Add files via upload

---
+++ function.js
@@ -0,0 +1,21 @@
+function SquareOfNumber(nmb) {
+    return nmb * nmb;
+}
+SquareOfNumber(5);
+
+function divisionOfNumber(nmb1, nmb2) {
+    return nmb1 / nmb2;
+}
+divisionOfNumber();
+
+alert(divisionOfNumber(7, 2));
+
+function cubeOfNumber(numb) {
+    return numb * numb * numb;
+}
+cubeOfNumber(5);
+
+function subtractTwoNumber(num1, nmb2) {
+    return numb1 - numb2;
+}
+console.log(subtractTwoNumber(100, 50));
